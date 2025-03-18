a073a756190aa5537984e59fae30b6eb303e57c0
ajaykumar1298 <ajaykumarak1209@gmail.com>
2025-03-19 00:06:15 +0530

    add skills

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiNodejs, DiGit, DiPython, DiBootstrap } from "react-icons/di";
+import { DiJavascript1, DiReact, DiNodejs, DiGit, DiPython, DiBootstrap, DiWordpress, DiCss3 } from "react-icons/di";
 import { SiMaterialui, SiGraphql, SiSolidity, SiRedux, SiMongodb, SiHtml5, SiTypescript } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { FaShopify } from "react-icons/fa";
@@ -20,19 +20,26 @@ const Techstack = () => {
             <Col xs={4} md={2} className="tech-icons">
                 <SiRedux />
             </Col>
-
             <Col xs={4} md={2} className="tech-icons">
                 <FaShopify />
             </Col>
+            
             <Col xs={4} md={2} className="tech-icons">
                 <SiHtml5 />
             </Col>
             <Col xs={4} md={2} className="tech-icons">
-                <SiTailwindcss />
+                <DiWordpress />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <DiCss3 />
             </Col>
             <Col xs={4} md={2} className="tech-icons">
                 <DiBootstrap />
             </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiTailwindcss />
+            </Col>
+            
 
 
             
